6f1866c15c2fbb71c66f5c022385a83b877988d9
Vy-Shane Xie <s@vyshane.com>
2020-05-20 22:52:42 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# pbts-grpc-transcoder
+
+pbts-grpc-transcoder is a TypeScript library that provides gRPC to HTTP/1 & JSON transcoding for the protobuf.js [library
+](https://github.com/protobufjs/protobuf.js).
+
---
+++ src/executor/index.ts
@@ -0,0 +1,7 @@
+// Copyright (c) 2020 WiseTime. All rights reserved.
+//
+// This is the API for the grpc/executor module.
+// The module allows calling RPCs via HTTP/1 and JSON.
+
+export { default, emptyRequestConfig } from "./executor"
+export * from "./retry"
